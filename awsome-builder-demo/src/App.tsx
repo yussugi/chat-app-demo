66722932d1a4e881e34dce0a6854eadaa8e44d9e
yussugi <yus-sugiyama@uninosushi.com>
2021-10-02 19:57:34 +0900

    create chat app MVP

--- awsome-builder-demo/src/App.tsx
+++ awsome-builder-demo/src/App.tsx
@@ -1,31 +1,75 @@
-import React from 'react'
-import './App.css'
-import Amplify from 'aws-amplify'
+import React, { useState } from 'react'
+import Amplify, { Auth } from 'aws-amplify'
+import { AmplifyAuthenticator, AmplifySignUp } from '@aws-amplify/ui-react'
 import {
-  AmplifyAuthenticator,
-  AmplifySignUp,
-  AmplifySignOut,
-} from '@aws-amplify/ui-react'
-import { AuthState, onAuthUIStateChange } from '@aws-amplify/ui-components'
+  AuthState,
+  onAuthUIStateChange,
+  CognitoUserInterface,
+} from '@aws-amplify/ui-components'
 import awsconfig from './aws-exports'
+import Content from './Content'
+import { RecoilRoot } from 'recoil'
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import ExitToAppIcon from '@material-ui/icons/ExitToApp'
 
 Amplify.configure(awsconfig)
 
-const AuthStateApp = () => {
-  const [authState, setAuthState] = React.useState<any>()
-  const [user, setUser] = React.useState<any>()
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+    },
+    toolBar: {
+      display: 'flex',
+    },
+    signOut: {
+      marginLeft: 'auto',
+      display: 'flex',
+    },
+  }),
+)
+
+const App = () => {
+  const classes = useStyles()
+  const [authState, setAuthState] = useState<AuthState>()
+  const [user, setUser] = useState<CognitoUserInterface | undefined>()
 
   React.useEffect(() => {
     return onAuthUIStateChange((nextAuthState, authData) => {
-      setAuthState(nextAuthState)
-      setUser(authData)
+      setAuthState(nextAuthState as AuthState)
+      setUser(authData as CognitoUserInterface)
     })
   }, [])
 
+  const handleClick = () => {
+    Auth.signOut()
+  }
+
   return authState === AuthState.SignedIn && user ? (
-    <div className="App">
-      <div>Hello, {user.username}</div>
-      <AmplifySignOut />
+    <div>
+      <RecoilRoot>
+        <AppBar className={classes.appBar}>
+          <Toolbar className={classes.toolBar}>
+            <Typography variant="h6" noWrap>
+              ChatApp
+            </Typography>
+            <div onClick={handleClick} className={classes.signOut}>
+              <IconButton
+                aria-label="display more actions"
+                edge="end"
+                color="inherit"
+              >
+                <ExitToAppIcon />
+              </IconButton>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Content userName={user.username} />
+      </RecoilRoot>
     </div>
   ) : (
     <AmplifyAuthenticator>
@@ -41,4 +85,4 @@ const AuthStateApp = () => {
   )
 }
 
-export default AuthStateApp
+export default App
---
+++ awsome-builder-demo/src/Content.tsx
@@ -0,0 +1,123 @@
+import React, { useEffect } from 'react'
+import { useRecoilState } from 'recoil'
+import { postListState, messageState, PostState } from './recoil/ChatState'
+import { API, graphqlOperation } from 'aws-amplify'
+import { GraphQLResult } from '@aws-amplify/api'
+import { listPostsSortedByCreatedAt } from './graphql/queries'
+import { createPost } from './graphql/mutations'
+import { onCreatePost } from './graphql/subscriptions'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import Chip from '@material-ui/core/Chip'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
+import { CreatePostMutation, ListPostsSortedByCreatedAtQuery } from './API'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      paddingTop: theme.spacing(10),
+      paddingBottom: theme.spacing(10),
+      backgroundColor: 'white',
+    },
+    input: {
+      display: 'flex',
+    },
+    myMessage: {
+      display: 'flex',
+      justifyContent: 'flex-start',
+    },
+    otherMessage: {
+      display: 'flex',
+      justifyContent: 'flex-end',
+    },
+  }),
+)
+
+interface ContentProps {
+  userName?: string
+}
+
+const Content = (props: ContentProps) => {
+  const classes = useStyles()
+  const [posts, setPosts] = useRecoilState(postListState)
+  const [message, setMessage] = useRecoilState(messageState)
+
+  const handleClick = () => {
+    postPost()
+  }
+
+  const postPost = async () => {
+    const post = (await API.graphql(
+      graphqlOperation(createPost, {
+        input: { message: message, owner: 'chat', user: props.userName },
+      }),
+    )) as GraphQLResult<CreatePostMutation>
+    const postData = post.data?.createPost as PostState
+    setPosts([...posts, postData])
+    setMessage('')
+  }
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(event.target.value)
+  }
+
+  useEffect(() => {
+    async function getPosts() {
+      const res = (await API.graphql(
+        graphqlOperation(listPostsSortedByCreatedAt, { owner: 'chat' }),
+      )) as GraphQLResult<ListPostsSortedByCreatedAtQuery>
+      const postData = res?.data?.listPostsSortedByCreatedAt
+        ?.items as PostState[]
+      setPosts(postData)
+    }
+    getPosts()
+  }, [setPosts])
+
+  useEffect(() => {
+    // @ts-ignore
+    const subscription = API.graphql(graphqlOperation(onCreatePost)).subscribe({
+      next: (eventData: any) => {
+        const post = eventData.value.data.onCreatePost
+        if (post !== undefined && post.user !== props.userName) {
+          setPosts([...posts, post])
+        }
+      },
+    })
+    return () => subscription.unsubscribe()
+  }, [posts])
+
+  const postList: JSX.Element[] = []
+
+  for (const post of posts) {
+    if (post.user === props.userName) {
+      postList.push(
+        <ListItem key={post.id} className={classes.myMessage}>
+          <Chip label={post.message}></Chip>
+        </ListItem>,
+      )
+    } else {
+      postList.push(
+        <ListItem key={post.id} className={classes.otherMessage}>
+          <Chip label={post.message}></Chip>
+        </ListItem>,
+      )
+    }
+  }
+
+  return (
+    <Container maxWidth="lg" className={classes.container}>
+      <div className={classes.input}>
+        <TextField value={message} onChange={handleChange} />
+        <Button variant="contained" color="secondary" onClick={handleClick}>
+          登録する
+        </Button>
+      </div>
+      <List>{postList}</List>
+    </Container>
+  )
+}
+
+export default Content
